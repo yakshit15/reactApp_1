071efb026e0cb19f46562f10c550f8d1b3dabfd7
Shravan Chouti <schouti@cisco.com>
2020-11-27 15:48:33 +0530

    Added images based on salutation on time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import night from './images/night.jpeg';
+import morning from './images/morning.jpeg';
+import afternoon from './images/afternoon.jpeg';
+import evening from './images/evening.jpeg';
 
 function App() {
+  const imgStyle = {
+    width: '50%',
+    height: '100 px',
+    pointerEvents: 'none'
+  }
+  const textStyle = {
+    position: 'absolute',
+    fontSize: '44px',
+    bottom: '230px',
+    left: '142px',
+  }
+  const style = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white'
+  }
+  let currentTime = new Date().getHours();
+  let message, img;
+  if (currentTime >= 4 && currentTime <= 11) {
+    message = 'Morning'
+    img = morning
+  }
+  else if (currentTime >= 12 && currentTime <= 16) {
+    message = 'Afternoon';
+    img = afternoon
+  }
+  else if ((currentTime >= 17 && currentTime <= 20)) {
+    message = 'Evening';
+    img = evening
+  }
+  else if ((currentTime >= 21 && currentTime <= 3)) {
+    message = 'Night';
+    img = night
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <img src={img} style={imgStyle} alt="logo" />
+      <p style={textStyle}>
+        Good {message}
+      </p>
     </div>
   );
 }
